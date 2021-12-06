c152c74b4f32dc87e91972d881596a13504116e7
shauryajain2067 <77152865+shauryajain2067@users.noreply.github.com>
2021-12-06 17:00:57 +0530

    Add files via upload

--- game_quizz.js
+++ game_quizz.js
@@ -11,7 +11,7 @@ document.getElementById("player_answer").innerHTML="Answer  Turn - " + player2_n
 function send(){
     var number1=document.getElementById("number1").value;
     var number2=document.getElementById("number2").value;
-    var actual_answer=parseInt(number1) * parseInt(number2);
+     actual_answer=parseInt(number1) * parseInt(number2);
     var question_number="<h4>" + number1 + "X" + number2 + "</h4>";
     var input_box="<br>Answer: <input type='text' id='input_check_box'>";
     var check_button="<br><br><button class='btn btn-info'  onclick='check()'>CHECK</button>"
@@ -19,4 +19,37 @@ function send(){
     document.getElementById("output").innerHTML = row;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
+}
+var question_player="player1";
+var answer_player="player2";
+var update_player1_score=0;
+var update_player2_score
+function check(){
+    get_answer=document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer){
+        if(answer_player == "player1"){
+update_player1_score=player1_score+1;
+document.getElementById("player1_score").innerHTML=update_player1_score;
+}
+else{
+    update_player2_score=player2_score+1;
+    document.getElementById("player2_score").innerHTML=update_player2_score;
+}
+}
+if(question_player == "player1"){
+question_player="player2";
+document.getElementById("player_question").innerHTML="Question Turn  - " + player2_name;
+}
+else{
+    question_player="player1";
+    document.getElementById("player_question").innerHTML="Question Turn  - " + player1_name;  
+}
+if(answer_player == "player1"){
+    answer_player="player2";
+    document.getElementById("player_answer").innerHTML="answer Turn  - " + player2_name;
+    }
+    else{
+        answer_player="player1";
+        document.getElementById("player_answer").innerHTML="Answer Turn - " + player1_name;  
+    }
 }
